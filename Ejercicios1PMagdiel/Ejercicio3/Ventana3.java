62c74879207de738f5c75a19e90e466ea1aafa4f
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2022-02-15 22:34:01 -0600

    holis

--- Ejercicios1PMagdiel/Ejercicio3/Ventana3.java
+++ Ejercicios1PMagdiel/Ejercicio3/Ventana3.java
@@ -3,5 +3,6 @@ package Ejercicio3;
 public class Ventana3 {
     public static void main(String[]args){
         System.out.println("Aqui va el ejercicio 3");
+        System.out.println("Aqui va el ejercicio 3");
     }
 }
