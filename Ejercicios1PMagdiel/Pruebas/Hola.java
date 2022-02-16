b86c8a880c800cf0a75e195490be14dfcc449efd
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2022-02-16 06:48:10 -0600

    buenos dias

--- Ejercicios1PMagdiel/Pruebas/Hola.java
+++ Ejercicios1PMagdiel/Pruebas/Hola.java
@@ -4,5 +4,7 @@ public class Hola {
     public static void main(String[]args){
         System.out.println("ME la pela visual");
         System.out.println("ME la pela visual");
+        System.out.println("ME la pela visual");
+        System.out.println("ME la pela visual");
     }
 }
