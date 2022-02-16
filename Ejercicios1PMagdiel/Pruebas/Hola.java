015682a7745a2a08ec5628222c968f907d550543
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2022-02-15 21:49:41 -0600

    hola

--- Ejercicios1PMagdiel/Pruebas/Hola.java
+++ Ejercicios1PMagdiel/Pruebas/Hola.java
@@ -3,5 +3,6 @@ package Pruebas;
 public class Hola {
     public static void main(String[]args){
         System.out.println("ME la pela visual");
+        System.out.println("ME la pela visual");
     }
 }
