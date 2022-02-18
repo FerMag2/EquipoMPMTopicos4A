c7571e785a0e8a3c3381eb3a58aeec6fe244c993
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2022-02-17 21:38:51 -0600

    ventana2

--- Ejercicios1PMagdiel/Ejercicio1/Ventana2.java
+++ Ejercicios1PMagdiel/Ejercicio1/Ventana2.java
@@ -5,13 +5,13 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class Ventana extends Frame implements ActionListener{
+public class Ventana2 extends Frame implements ActionListener{
 	Button aceptar;
 	TextField text;
 	Panel central;
 	TextArea txt;
 	Button igual;
-	public Ventana() {
+	public Ventana2() {
 		aceptar= new Button("Aceptar");
 		aceptar.addActionListener(this);
 		aceptar.setBackground(new Color(125,125,125));
@@ -59,6 +59,6 @@ public class Ventana extends Frame implements ActionListener{
 		});
 	}
 	public static void main(String args[]) {
-		Ventana miventana = new Ventana();
+		Ventana2 miventana = new Ventana2();
 	}
 }
