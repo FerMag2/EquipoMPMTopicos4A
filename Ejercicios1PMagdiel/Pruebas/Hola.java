ded24a1d7a836e60618e0d8a93066bbba1b858c7
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2022-02-21 07:51:41 -0600

    hola

--- Ejercicios1PMagdiel/Pruebas/Hola.java
+++ Ejercicios1PMagdiel/Pruebas/Hola.java
@@ -7,5 +7,10 @@ public class Hola {
         System.out.println("hola visual");
         System.out.println("hola visual");
         System.out.println("hola visual");
+        System.out.println("hola visual");
+        System.out.println("hola visual");
+        System.out.println("hola visual");
+        System.out.println("hola visual");
+        System.out.println("hola visual");
     }
 }
