fe4a875df0d2aad6b79c35c81c2a964645c46e8f
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2022-03-12 09:52:11 -0600

    hola

--- Ejercicios1PMagdiel/Pruebas/Hola.java
+++ Ejercicios1PMagdiel/Pruebas/Hola.java
@@ -4,13 +4,13 @@ public class Hola {
     public static void main(String[]args){
         System.out.println("hola visual");
         System.out.println("hola visual");
+        System.out.println("hola ");
         System.out.println("hola visual");
         System.out.println("hola visual");
         System.out.println("hola visual");
         System.out.println("hola visual");
         System.out.println("hola visual");
-        System.out.println("hola visual");
-        System.out.println("hola visual");
+        System.out.println("hola Magdiel");
         System.out.println("hola visual");
     }
 }
