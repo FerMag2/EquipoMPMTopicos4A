e7ae7bb4f0a416e160c2e533b322353cbf8cfb67
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2022-03-13 08:04:43 -0600

    Ejercicio4

---
+++ Ejercicios1PMagdiel/Ejercicio4/Main.java
@@ -0,0 +1,77 @@
+package Ejercicio4;
+
+import java.awt.Frame;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.*;
+public class Main extends Frame implements MouseMotionListener, MouseListener{
+	Panel p;
+	Mundo d;
+	public Main() {
+		p= new Panel();
+		d = new Mundo();
+		p.setLayout(new BorderLayout());
+		p.add(d,BorderLayout.CENTER);
+		this.setLayout(new BorderLayout());
+		this.add(p,BorderLayout.CENTER);
+		this.setSize(500,400);
+		this.setVisible(true);
+		this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+	}
+	
+	public static void main(String args[]) {
+		Main canvasi= new Main();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
---
+++ Ejercicios1PMagdiel/Ejercicio4/Mundo.java
@@ -0,0 +1,24 @@
+package Ejercicio4;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Mundo extends Canvas{
+	int x,y;
+	int w,h;
+	public Mundo() {
+		x=20;
+		y=28;
+		w=h=100;
+	}
+	public void paint(Graphics g) {
+		
+			g.setColor(Color.black);
+			g.drawRect(x, y, w, h);
+			g.fillOval(x, y, w, h);
+			g.setColor(Color.white);
+			g.drawString("Hola mundo", x,(y+h/2));
+	}
+}
+
