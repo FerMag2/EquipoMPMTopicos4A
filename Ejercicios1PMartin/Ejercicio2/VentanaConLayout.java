8cd53d3eee5dfbc79204a781d9145e793b3c0585
MartinF-20 <73145443+MartinF-20@users.noreply.github.com>
2022-03-20 13:19:39 -0600

    main

--- Ejercicios1PMartin/Ejercicio2/VentanaConLayout.java
+++ Ejercicios1PMartin/Ejercicio2/VentanaConLayout.java
@@ -1,13 +1,21 @@
 package Ejercicio2;
 
-import java.awt.*;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Panel;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
-import java.awt.event.WindowEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 public class VentanaConLayout extends Frame implements ActionListener{
+
     Button btn;
     Button btn1;
     Button btn2;
@@ -19,42 +27,45 @@ public class VentanaConLayout extends Frame implements ActionListener{
     TextArea txt;
 
     public VentanaConLayout(){
-        btn = new Button("Salir del programa");
-        btn1 = new Button("");
-        btn2 = new Button("");
+
+        btn = new Button("Cerrar");
+        btn1 = new Button("8");
+        btn2 = new Button("6");
         btns = new Button[10];
-        
-        for (int i=0; i<btns.length; i++){
-            btns[i] = new Button(i + "");
+        for (int i=0;i<btns.length; i++){
+            btns[i] = new Button(i +" ");
             btns[i].addActionListener(this);
         }
-        btn.addActionListener(this);
+
         p1 = new Panel();
         p2 = new Panel();
         p3 = new Panel();
         p4 = new Panel();
 
-        p4.add(btn1);
-        p4.add(btn2);
         p4.setSize(200,100);
-        p4.setVisible(true);
+		p4.add(btn1);
+		p4.add(btn2);
 
-        txt = new TextArea();
+        btn1.setBounds(5, 5, 20, 20);
+        btn2.setBounds(5,5, 20, 20);
+
+		p4.setVisible(true);
 
         this.setLayout(new BorderLayout());
 
         p1.setLayout(new GridLayout(3,2));
-        for (int i=0; i<6;i++){
-            p1.add(btns[i]);
-        }
+			for (int i=0; i<6;i++){
+	            p1.add(btns[i]);
+	        }
 
         p2.setLayout(new FlowLayout());
-        for (int i=6; i<btns.length;i++){
-            p2.add(btns[i]);
-        }
+			for (int i=6; i<btns.length;i++){
+	            p2.add(btns[i]);
+	        }
 
         p3.setLayout(new CardLayout());
         p3.add(btns[9]);
+
         p4.setLayout(null);
 
         this.add(p1, BorderLayout.NORTH);
@@ -62,57 +73,41 @@ public class VentanaConLayout extends Frame implements ActionListener{
         this.add(p3, BorderLayout.CENTER);
         this.add(p4, BorderLayout.EAST);
         this.add(btn, BorderLayout.WEST);
-        
-        btn1.setBounds(5, 5, 20, 20);
-        btn2.setBounds(5, 5, 20, 20);
 
-        
-        btn.addActionListener(this);
-        btn1.addActionListener(this);
-        btn2.addActionListener(this);
-        this.setSize(800,200);
+        this.setSize(400, 200);
         this.setVisible(true);
+        
+        //this.addWindowListener(new WindowAdapter());
 
+        btn.addActionListener(this);
+		btn1.addActionListener(this);
+	    btn2.addActionListener(this);
 
-        this.addWindowListener(new WindowAdapter() {
+    }
+
+    public void cerrarVentana() {
+		addWindowListener((WindowListener) new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
+	}
 
-        //txt = new TextArea();
-        this.setLayout(new BorderLayout());
-        p1.setLayout(new GridLayout());
-        p2.setLayout(new FlowLayout());
-        p3.setLayout(new CardLayout());
+    public static void main(String[] args) {
 
-        this.add(p1, BorderLayout.NORTH);
-        this.add(p2, BorderLayout.SOUTH);
-        this.add(p3, BorderLayout.CENTER);
-        this.add(btn, BorderLayout.WEST);
+        VentanaConLayout v1 = new VentanaConLayout();
 
-        this.setSize(400,200);
-        this.setVisible(true);
     }
 
-    //Metodo para cerra el Frame con la X
-    public void CerrarVentana(){
-        addWindowListener((WindowListener)new WindowAdapter(){
-            public void windowClosing(WindowEvent e){
-        
-        System.exit(0);
-        }
-        });
-        }
-
-    public static void main (String args []){
-        VentanaConLayout VCL = new VentanaConLayout();
-    }
-
+    @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==btn) System.exit (0);
-            else{
-                System.out.println(e.getActionCommand());
-            }
+        // TODO Auto-generated method stub
+        if (e.getSource()==btn) System.exit(0);
+        
+        else {
+            System.out.println(e.getActionCommand());
         }
+        
     }
+
+}
---
+++ Ejercicios1PMartin/Ejercicio4/Main.java
@@ -0,0 +1,135 @@
+package Ejercicio4;
+
+import Ejercicio4.Mundo;
+import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.BorderLayout;
+import java.awt.event.*;
+import java.util.Random;
+
+public class Main extends Frame implements MouseListener, MouseMotionListener, KeyListener{
+    Panel p;
+    Mundo mundo;
+
+    Random r = new Random();
+
+    public Main(){
+        p = new Panel();
+        this.setLayout(new BorderLayout());
+        p.setLayout(new BorderLayout());
+        mundo = new Mundo();
+        p.add(mundo);
+        this.addKeyListener(this);
+        p.addKeyListener(this);
+        mundo.addMouseListener(this);
+        mundo.addMouseMotionListener(this);
+        mundo.addKeyListener(this);
+        this.add(p, BorderLayout.CENTER);
+        this.setSize(500,400);
+        this.setVisible(true);
+
+    }
+
+    public static void main(String args []){
+        new Main().addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Dragged");
+        mundo.setX(e.getX());
+        mundo.setY(e.getY());
+        mundo.repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Moved");
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Clicked");
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+                System.out.println("Pressed");
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Released");
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Entered");
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Exit");
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Typed");
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Key Pressed");
+
+        switch (e.getKeyChar()){
+            case 'w':
+            case KeyEvent.VK_W:
+            mundo.setY(mundo.getY()-1);
+                break;
+
+            case 's':
+            case KeyEvent.VK_S:
+            mundo.setY(mundo.getY()+1);
+                break;
+            
+            case 'a':
+            case KeyEvent.VK_A:
+            mundo.setX(mundo.getX()-1);
+                break;
+            
+            case 'd':
+            case KeyEvent.VK_D:
+            mundo.setX(mundo.getX()+1);
+                break;
+        }
+        mundo.setSprite();
+        mundo.repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Key Released");
+
+    }
+}
